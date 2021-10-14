0f1a9e1407dde9a389caf0fb33414735bd0474fd
Jordan Esh <esh.jordan@gmail.com>
2021-10-14 14:41:29 +1100

    Added population data and NTAs to lookup

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ let vega_lite_definition_2 = "js/nyc_rideshare-2.vg.json"
 vegaEmbed('#visualisation-1', vega_lite_definition_1).then(result => {}).catch(console.error);
 
 vegaEmbed('#visualisation-2', vega_lite_definition_2).then(result => {}).catch(console.error);
+
+// https://github.com/fivethirtyeight/uber-tlc-foil-response
+// https://data.cityofnewyork.us/City-Government/New-York-City-Population-By-Neighborhood-Tabulatio/swpk-hqdp
+// https://data.cityofnewyork.us/City-Government/NTA-map/d3qk-pfyz
--- util/extract_neighborhoods.py
+++ util/extract_neighborhoods.py
@@ -1,21 +1,78 @@
 #!/usr/bin/env python3
 
 import json
+import csv
+from shapely.geometry import Polygon, Point
 
-with open('./js/nyc-neighbourhoods.geo.json') as f:
-    geojson = json.load(f)
+with open('./data/nyc-neighbourhoods.geojson') as f:
+    my_geojson = json.load(f)
 
-boroughLookup = {}
+with open('./data/nta-map.geojson') as f:
+    nta_geojson = json.load(f)
 
-for feature in geojson['features']:
+# boroughLookup = {}
+
+# for feature in my_geojson['features']:
+#     neighborhood = feature['properties']['neighborhood']
+#     boroughCode = feature['properties']['boroughCode']
+#     borough = feature['properties']['borough']
+#     neighborhoodId = feature['properties']["@id"]
+
+#     if neighborhood not in boroughLookup:
+#         boroughLookup[neighborhood] = [borough, boroughCode, neighborhoodId]
+
+
+# with open('./neighborhood-lookup.json', 'w', encoding='utf-8') as jsonf:
+#     jsonf.write(json.dumps(boroughLookup, check_circular=False, indent=None))
+
+neighborhoodLookup = {}
+
+for feature in my_geojson['features']:
     neighborhood = feature['properties']['neighborhood']
     boroughCode = feature['properties']['boroughCode']
     borough = feature['properties']['borough']
     neighborhoodId = feature['properties']["@id"]
 
-    if neighborhood not in boroughLookup:
-        boroughLookup[neighborhood] = [borough, boroughCode, neighborhoodId]
+    neighborhoodLookup[neighborhood] = []
+
+    for nta in nta_geojson['features']:
+        # "ntacode","shape_area","ntaname","shape_leng","boroname","borocode","countyfips"
+        ntaName = nta['properties']['ntaname']
+        ntaId = nta['properties']['ntacode']
+        ntaCountyFips = nta['properties']['countyfips']
+        ntaArea = nta['properties']['shape_area']
+
+        neighborhood_centre = Polygon(feature['geometry']['coordinates'][0]).centroid
+        for elem in nta['geometry']['coordinates']:
+            for poly in elem:
+                nta_polygon = Polygon(poly)
+
+                if nta_polygon.contains(neighborhood_centre):
+                    if ntaName not in neighborhoodLookup[neighborhood]:
+                        neighborhoodLookup[neighborhood].append([ntaName, ntaId, ntaArea, ntaCountyFips])
 
 
-with open('./neighborhood-lookup.json', 'w', encoding='utf-8') as jsonf:
-    jsonf.write(json.dumps(boroughLookup, check_circular=False, indent=None))
+with open('data/neighborhood-lookup.csv', newline='', encoding='utf-8') as in_file:
+    neighborhoodReader = csv.reader(in_file)
+
+    with open('data/neighborhood-lookup-nta.csv', 'w', newline='', encoding='utf-8') as out_file:
+        csvWriter = csv.writer(out_file)
+
+        header = True
+        for row_data in neighborhoodReader:
+            if header:
+                csvWriter.writerow(row_data + ["l_nta_name", "l_nta_id", "l_nta_area", "l_nta_county_fips"])
+                header = False
+                continue
+            
+            neighborhood = row_data[0]
+            if len(neighborhoodLookup[neighborhood]) == 1:
+                csvWriter.writerow(row_data + neighborhoodLookup[neighborhood][0])
+
+
+for key, value in neighborhoodLookup.items():
+    if len(value) != 1:
+        print(key, value)
+
+# with open('./neighborhood-lookup.json', 'w', encoding='utf-8') as jsonf:
+#     jsonf.write(json.dumps(boroughLookup, check_circular=False, indent=None))
